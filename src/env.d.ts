b2beb66265004bea916c14aa324a29c10a1209dd
Ste <stedamb@gmail.com>
2026-03-08 22:18:05 +0100

    added google verification id

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,3 +3,11 @@ export interface Route {
   label: string;
   icon?: string;
 }
+
+interface ImportMetaEnv {
+  readonly PUBLIC_GOOGLE_SITE_VERIFICATION: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
